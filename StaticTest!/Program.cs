83cee3f8a1130a88f1aa08d525fbc42fbb59172f
vadimAbdullaev <amdarisdev@gmail.com>
2017-12-09 13:29:37 +0200

    added some factory for static demo

---
+++ StaticTest!/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticTest_
+{
+    class Manufacory
+    {
+        public string Name { get; set; }
+
+        public Manufacory(string name)
+        {
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+    class ManufactoryFactory
+    {
+        public Manufacory Create(string name)
+        {
+            return new Manufacory(name);
+        }
+    }
+    class StaticClass
+    {
+        public static ManufactoryFactory Factory => new ManufactoryFactory();
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory1 = StaticClass.Factory.Create("factory1");
+            Console.WriteLine(factory1);
+
+            var factory2 = StaticClass.Factory.Create("factory2");
+            Console.WriteLine(factory2);
+        }
+    }
+}
